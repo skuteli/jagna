c87c42329290dafe85f65ce86435f64bf588d295
Oskar Skuteli <oskar.skuteli@nielsen.com>
2019-06-13 18:38:56 +0200

    initial

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import 'antd/dist/antd.css'
+import './styles.css'
+import React, { Component, Fragment } from 'react'
+import { render } from 'react-dom'
+ 
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import lodash from 'lodash'
+import { Icon } from 'antd'
+import data from './data'
+import Header from './Header'
+import { Grid, Slug, Fade } from 'mauerwerk'
+
+import Main from './components/Main';
+import NotFound from './components/NotFound';
+
+const App = () => {
+  return (
+    <Router>
+
+      {/*
+        <Match exactly pattern="/" component={Main} />
+        <Match exactly pattern="/contact" component={Contact} />
+        <Match pattern="/works/" component={Works} />
+      */}
+         
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route component={NotFound} />
+        </Switch>
+      
+    </Router>
+  )
+}
+
+render(<App />, document.getElementById('root'))
